ed212ac35ebc218f6ddc43b807b33c72d650a903
bcleenders <bcleenders@gmail.com>
2015-07-09 17:25:15 +0200

    Start show course screen

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,14 +6,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
     sign_in(:user, @user) unless user_signed_in?
     user_session["supervisor"] = params[:roles].include? 'Instructor'
+
+    # Supervisors are redirected to the course page
     if supervisor_signed_in?
       @course = Course.find_or_create_by(isis_id: params[:context_id]) do |c|
         c.name = params[:context_title]
       end
       session[:context_id] = params[:context_id]
       @course.supervisors << current_supervisor unless @course.supervisors.include? @user
-      redirect_to edit_course_path(@course)
+      redirect_to course_path(@course)
     end
+
+    # Students are redirected to the course registration page
     if student_signed_in?
       @course = Course.find_by(isis_id: params[:context_id])
       raise ActiveRecord::RecordNotFound unless @course
