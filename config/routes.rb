d6f42ffd125848cc32e92d164f94c9ef9adfdb99
bcleenders <bcleenders@gmail.com>
2015-07-14 12:02:14 +0200

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'static#root'
 
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :courses, only: [:show, :edit, :update] do
