ef0760b0b9e431d14f4b2f999fd75f1033949ed4
bcleenders <bcleenders@gmail.com>
2015-09-01 15:59:31 +0200

    Add skills to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,14 @@ require 'factory_girl_rails'
 require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
-#FactoryGirl.create :course, :with_associations, :with_preferences, isis_id: 5036
+#FactoryGirl.create :course, :with_associations, :with_preferences, isis_id: 1
 #FactoryGirl.create_list :course, 10, :with_associations
+
+seed_file = Rails.root.join('db', 'skills.yml')
+skills = YAML::load_file(seed_file)
+counter = 0
+skills.each do |skill|
+	Skill.create!(:name => skill) do |n|
+	  puts "Create skill #{counter += 1} with name: #{skill}"
+	end
+end
