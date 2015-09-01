96ef97b2d4bd78def51abcb8cfce6940a826d3d7
bcleenders <bcleenders@gmail.com>
2015-09-01 15:42:58 +0200

    Remove default course from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require 'factory_girl_rails'
 require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
-FactoryGirl.create :course, :with_associations, :with_preferences, isis_id: 5036
-FactoryGirl.create_list :course, 10, :with_associations
+#FactoryGirl.create :course, :with_associations, :with_preferences, isis_id: 5036
+#FactoryGirl.create_list :course, 10, :with_associations
